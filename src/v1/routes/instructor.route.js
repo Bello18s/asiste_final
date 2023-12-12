1485acf2516248ddc84d081b481832f4c78044a7
Bello18s <105251905+Bello18s@users.noreply.github.com>
2023-12-12 11:42:02 -0500

    Update instructor.route.js

--- src/v1/routes/instructor.route.js
+++ src/v1/routes/instructor.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../../controller/instructorController');
 
 router.post('/', controller.postInstructor);
-router.get('/:id', controller.getInstructor);
+router.get('/', controller.getInstructor);
 router.put('/:id', controller.putInstructor);
 router.delete('/:id', controller.deleteInstructor);
 
